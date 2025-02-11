2e595b1546b47330d7c012b2bd77845e7872520d
duywin <duywow12345@gmail.com>
2025-02-11 21:53:56 +0700

    Temporal Main User

---
+++ app/src/main/java/com/example/readingapp/user/userMain.java
@@ -0,0 +1,16 @@
+package com.example.readingapp.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.readingapp.R;
+
+public class userMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_main);
+    }
+}
